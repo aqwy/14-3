d1b2319359df0bf4f6fe1e9fd77ed0595c2342ca
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-14 14:25:20 +0300

    сртп

---
+++ 14-3/Graph.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_3
+{
+    class Graph
+    {
+        private readonly int MAX_VERTS = 20;
+        private readonly int INFINITY = 1000000;
+        private Vertex[] vertexList;
+        private int nVerts;
+        private int[,] adjMat;
+        public Graph()
+        {
+            adjMat = new int[MAX_VERTS, MAX_VERTS];
+            vertexList = new Vertex[MAX_VERTS];
+            nVerts = 0;
+            for (int i = 0; i < MAX_VERTS; i++)
+                for (int j = 0; j < MAX_VERTS; j++)
+                    adjMat[i, j] = INFINITY;
+        }
+        public void addVertex(char lab)
+        {
+            vertexList[nVerts++] = new Vertex(lab);
+        }
+        public void addEdge(int start, int end, int weight)
+        {
+            adjMat[start, end] = weight;
+        }
+        public void algFloyd()
+        {
+            for (int i = 0; i < nVerts; i++)
+            {
+                for (int j = 0; j < nVerts; j++)
+                {
+                    if (adjMat[i, j] < INFINITY)
+                    {
+                        for (int k = 0; k < nVerts; k++)
+                        {
+                            int temp = adjMat[k, i] + adjMat[i, j];
+                            if (adjMat[k, j] > temp)
+                                adjMat[k, j] = temp;
+                        }
+                    }
+                }
+            }
+        }
+        public void displayPaths()
+        {
+            for (int i = 0; i < nVerts; i++)
+                Console.Write("	" + vertexList[i].lable);
+
+            Console.WriteLine();
+            for (int j = 0; j < nVerts; j++) // display contents of sPath[]
+            {
+                Console.Write(vertexList[j].lable);
+                for (int i = 0; i < nVerts; i++)
+                {
+                    int temp = adjMat[j, i];
+                    if (temp != INFINITY)
+                        Console.Write("	" + adjMat[j, i]);
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- 14-3/Program.cs
+++ 14-3/Program.cs
@@ -10,6 +10,30 @@ namespace _14_3
     {
         static void Main(string[] args)
         {
+            Graph theGraph = new Graph();
+            theGraph.addVertex('A'); // 0 
+            theGraph.addVertex('B'); // 1
+            theGraph.addVertex('C'); // 2
+            theGraph.addVertex('D'); // 3
+            theGraph.addVertex('E'); // 4
+
+            theGraph.addEdge(0, 1, 50); // AB 50
+            theGraph.addEdge(0, 3, 80); // AD 80
+            theGraph.addEdge(1, 2, 60); // BC 60
+            theGraph.addEdge(1, 3, 90); // BD 90
+            theGraph.addEdge(2, 4, 40); // CE 40
+            theGraph.addEdge(3, 2, 20); // DC 20
+            theGraph.addEdge(3, 4, 70); // DE 70
+            theGraph.addEdge(4, 1, 50); // EB 50
+
+            Console.WriteLine("before Floyd");
+            theGraph.displayPaths();
+            theGraph.algFloyd();
+            Console.WriteLine("after Floyd");
+            theGraph.displayPaths();
+            Console.WriteLine();
+
+            Console.ReadLine();
         }
     }
 }
